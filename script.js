7482e6e0f77062db9df4fd3a519313f34c534811
Meyadur Rahman <100347980+MiyadurRahman@users.noreply.github.com>
2026-09-09 13:25:02 +0600

    Refactor navigation menu and enhance lesson loading functionality

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ const loadlesson=()=>{
     .then(response=> response.json())
     .then(json => displaylesson(json.data));
 }
+const loadlevelword = (lessonId) => {
+    const url = `https://openapi.programming-hero.com/api/level/${lessonId}`;
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => displaylevelword(data.data))
+        .catch((error) => console.error("Failed to load lesson:", error));
+
+}
+const displaylevelword=(words)=>{
+const wordcontainer= document.getElementById("word-container")
+wordcontainer.innerHTML="";
+
+words.forEach(word => {
+    const card=document.createElement("div")
+    card.innerHTML=`   `
+});
+
+wordcontainer.appendChild(card);
+
+}
 const displaylesson =(lessons)=>{
     // get the container & empty
     const levelcontainer=document.getElementById("level-container")
@@ -13,9 +34,9 @@ const displaylesson =(lessons)=>{
     // get intoo every lesson 
  for(let lesson of lessons){
     const btnDiv=document.createElement("div");
-    btnDiv.innerHTML=`<button class="btn btn-outline btn-primary">
+    btnDiv.innerHTML=`<button onclick="loadlevelword(${lesson.level_no})" class="btn btn-outline btn-primary">
     <i class="fa-solid fa-book-open-reader"></i>
-    Learn</button>`
+    Lesson-${lesson.level_no}</button>`
 
     levelcontainer.appendChild(btnDiv);
  }
